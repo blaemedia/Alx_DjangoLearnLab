c1bb0ede17a08dbeff79a8e6ff4887de80da7058
Emmanuel <blaemedia26@gmail.com>
2026-01-24 05:16:18 +0100

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -14,7 +14,6 @@ class BookAdmin(admin.ModelAdmin):
 
 
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
 
@@ -37,3 +36,7 @@ class CustomUserAdmin(UserAdmin):
             "fields": ("date_of_birth", "profile_photo"),
         }),
     )
+
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
